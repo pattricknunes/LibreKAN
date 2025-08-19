f9a1503cd0d8165c00e7e79ed647595c4672db70
Diego-Paes <diego.paes@gsuite.iff.edu.br>
2025-08-18 21:59:04 -0300

    correção la

--- librekan/src/main/java/br/edu/iff/ccc/librekan/librekan/model/Quadro.java
+++ librekan/src/main/java/br/edu/iff/ccc/librekan/librekan/model/Quadro.java
@@ -1,16 +1,16 @@
 package br.edu.iff.ccc.librekan.librekan.model;
 
-import jakarta.persistence.*;
+
 
 @Entity
 public class Quadro {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     private String nome;
 
-    // Getters e setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
