511533b0c1d1dd77ec740e3162b68502e3b63245
pattricknunes <pattrick.nunes@gsuite.iff.edu.br>
2025-08-31 23:32:35 -0300

    .

--- librekan/src/main/java/br/edu/iff/ccc/librekan/librekan/repository/QuadroRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.iff.ccc.librekan.librekan.repository;
-
-import br.edu.iff.ccc.librekan.librekan.model.Quadro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuadroRepository extends JpaRepository<Quadro, Long> {
-}
